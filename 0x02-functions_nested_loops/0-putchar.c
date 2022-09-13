ffa32b7cbac6a202fe91775a393ee1faf18cacff
BIGRIZZY <oluwoleadebanjo2016@gmail.com>
2022-09-13 05:28:49 -0700

    zero

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,19 @@
 #include "main.h"
 /**
- * print_alphabet - Prints the alphabet in lowercase
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
-void print_alphabet(void)
+int main(void)
 {
-	char alpha;
-
-	for (alpha = 'a'; alpha <= 'z'; alpha++)
-	{
-		_putchar (alpha);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+	return (0);
 }
