4e2d5d0371817e82453cedc5f9c974e81ab207fb
BIGRIZZY <oluwoleadebanjo2016@gmail.com>
2022-09-14 02:29:50 -0700

    new

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef main_h
+#define main_h	
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
+#endif /* main_h */
+
