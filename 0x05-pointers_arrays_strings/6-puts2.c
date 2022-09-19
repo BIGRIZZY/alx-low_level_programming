74cb9b0b074841b7210c8b1ca15376177a1a0f4c
BIGRIZZY <oluwoleadebanjo2016@gmail.com>
2022-09-19 00:08:38 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints one char out of two of a string.
+ * @str: The string containing characters.
+ */
+void puts2(char *str)
+{
+	int i = 0, len = 0;
+
+	while (str[i++])
+		len++;
+
+	for (i = 0; i < len; i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
