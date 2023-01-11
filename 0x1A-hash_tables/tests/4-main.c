f501de1cf95f4e9ac40ca4407669f2151de88d26
BIGRIZZY <oluwoleadebanjo2016@gmail.com>
2023-01-11 04:54:43 -0800

    keys

---
+++ 0x1A-hash_tables/tests/4-main.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "../hash_tables.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+	hash_table_t *ht;
+	char *value;
+
+	ht = hash_table_create(1024);
+
